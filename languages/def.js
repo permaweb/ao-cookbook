17e52dd56c75ab2fbcdde81a9cc76ff3aa3caead
HelloRWA <dev@rwa-nft.com>
2024-03-07 13:56:50 +0800

    feat: add zh lang pack and fix locale load bug

--- languages/def.js
+++ languages/def.js
@@ -2,12 +2,20 @@
 import { createRequire } from "node:module";
 
 const enStrings = createRequire(import.meta.url)("./strings/en.json");
+const zhStrings = createRequire(import.meta.url)("./strings/zh.json");
 /**
  * Start adding new languages by making a new language object inside the array
  *
  * @see file://./README.md#add-a-new-language
  */
-const languages = [];
+const languages = [
+  {
+    display: "中文", // Name of the language displayed in UI
+    name: "中文", // Name of the language in English, used by OpenAI translation
+    code: "zh", // 2 letter language code (ISO 639‑1)
+    strings: zhStrings, // JSON object of translated UI element strings
+  },
+];
 
 const i18n_strs = languages.reduce((langs, currentLang) => {
   langs[currentLang.code] = currentLang.strings;
