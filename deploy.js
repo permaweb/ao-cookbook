d1c8f72728b76917194e2c1fefd4456774183067
Tyler Hall <tyler@hyper.io>
2024-08-07 17:08:24 +0000

    ci(ant): use ario sdk and ao process to update ANT

--- deploy.js
+++ deploy.js
@@ -1,7 +1,6 @@
 import { readFileSync } from "node:fs";
 
-import { WarpFactory, defaultCacheOptions } from "warp-contracts";
-import Arweave from "arweave";
+import { ANT, ArweaveSigner } from "@ar.io/sdk";
 
 function env(key) {
   if (!process.env[key]) {
@@ -21,24 +20,19 @@ const actions = {
      * The ao ANT Contract
      * See https://sonar.warp.cc/?#/app/contract/uOf4TMgQxdaSXgcZ778PZR13UQPKJoZVK2ZvLAE90Xg?network=mainnet%23
      */
-    const ANT = "uOf4TMgQxdaSXgcZ778PZR13UQPKJoZVK2ZvLAE90Xg";
+    // const ANT = "uOf4TMgQxdaSXgcZ778PZR13UQPKJoZVK2ZvLAE90Xg";
+    const ANT_PROCESS = "YFbfeqZMbPVGFjQ-PHJY-Y99JQu7O3Jdet06pJnD5iI";
 
-    const arweave = Arweave.init({
-      host: "arweave.net",
-      port: 443,
-      protocol: "https",
-    });
-    const warp = WarpFactory.forMainnet(defaultCacheOptions, true, arweave);
+    const signer = new ArweaveSigner(jwk);
+    const ant = ANT.init({ processId: ANT_PROCESS, signer });
 
-    // Update the ao ANT Contract to point to the new install script
-    const aoAntContract = warp.contract(ANT).connect(jwk);
-    const antUpdate = await aoAntContract.writeInteraction({
-      function: "setRecord",
-      subDomain: "cookbook",
-      transactionId: MANIFEST_ID,
-    });
-
-    console.log("Updated ao ANT record", antUpdate.interactionTx);
+    await ant
+      .setRecord({
+        undername: "cookbook",
+        transactionId: MANIFEST_ID,
+        ttlSeconds: 3600,
+      })
+      .then((res) => console.log("Updated ao ANT record", res.id));
 
     return MANIFEST_ID;
   },
