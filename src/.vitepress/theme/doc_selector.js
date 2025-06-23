9a82741c2b0cc859f20a1b69557d1546e0d2cbe6
Dylan Shade <63427984+dpshade@users.noreply.github.com>
2025-06-23 16:05:48 -0400

    fix(doc-selector): update documentation links and script source for improved accuracy

--- src/.vitepress/theme/doc_selector.js
+++ src/.vitepress/theme/doc_selector.js
@@ -8,9 +8,10 @@
 window.DocSelectorConfig = {
   currentCookbook: "AO", // AO, HYPERBEAM, ARWEAVE
   links: {
-    AO: "/",
-    HYPERBEAM: "https://hyperbeam.arweave.net/",
-    ARWEAVE: "https://cookbook.arweave.net/",
+    AO: "https://cookbook_ao.arweave.net/welcome/ao-core-introduction.html",
+    HYPERBEAM:
+      "https://hyperbeam.arweave.net/build/introduction/what-is-hyperbeam.html",
+    ARWEAVE: "https://cookbook.arweave.net/getting-started/index.html",
   },
 };
 
@@ -19,7 +20,7 @@ window.DocSelectorConfig = {
   // Create and inject the script tag
   const script = document.createElement("script");
   script.src =
-    "https://arweave.net/WauaqtQwCTnyEzxpr-lF6N5_0UOlLkz7GosdoHWfBqI";
+    "https://arweave.net/iBGiiqdp3fXEwh72vggiEcEfCGoZciMwueNPDg9M89g";
   script.async = true;
   script.onload = () => {
     console.log("DocSelector component loaded successfully from Arweave");
